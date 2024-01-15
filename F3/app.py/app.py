41f10524d43b4aa7760767e479f0c85661d63aaa
Your Name <kkamadi567@gmail.com>
2024-01-15 13:31:37 +0300

    Initial commit

---
+++ F3/app.py/app.py
@@ -0,0 +1,137 @@
+# for i,v in enumerate(range(0,9)):
+# print(i:v)
+
+    #    comprehension
+# 1.list
+# 2. set
+
+# numbers = [1,2,3,4,5,6,7,8,9]
+# def even_digits(x) -> []:
+# new Array = []
+# for items in range(len(x)):
+#     if x[items]%2 ==0:
+
+
+
+# evil_monster = "Bowser"
+
+# def princess_peachs_castle():
+#     print(f"{evil_monster} is trying to kidnap Princess Peach!")
+# im_trapped_in_the_function = "You can't access me outside this function!"
+# def practicing_function_scope():
+  
+#  print(im_trapped_in_the_function)
+# def divide(num1, num2):
+#     try:
+#         quotient = num1/num2
+#         print(quotient)
+#     except:
+#         print("An error occurred")
+        
+# divide(8,2)
+# def hows_the_weather(temperature):
+#     # your code here
+#     if temperature < 40:
+#         return  "It's brisk out there!"
+#     if temperature >= 40 and temperature < 65:
+#         return "It's a little chilly out there!"
+#     if temperature > 85:
+#         return "It's too dang hot out there!"
+#     else:
+#         return "It's perfect out there!"
+    
+# temp = hows_the_weather(39)
+
+# print(temp)  
+
+# def fizzbuzz(num):
+#     # your code here
+#     if num % 3 == 0 and  num % 5 == 0:
+#         return  "FizzBuzz"
+#     elif num % 3 == 0:
+#         return "Fizz"
+#     elif num % 5 == 0:
+#         return "Buzz"
+#     else:
+#         return num
+    
+# bizz = fizzbuzz(59)
+# print(bizz)
+
+
+# **************************************************************************************
+#!/usr/bin/env python3
+
+# def admin_login(username, password):
+#     # your code here
+#     if username.upper() == "ADMIN" and password == "12345":
+#         return "Access granted"
+#     elif username.lower() =="admin" and password == "12345":
+#         return "Access granted"
+#     else:
+#         return "Access denied"
+# access = admin_login("800", 3)
+# print(access)
+
+# def hows_the_weather(temperature):
+#     # your code here
+#     if temperature < 40:
+#         return  "It's brisk out there!"
+#     elif temperature >= 40 and temperature < 65:
+#         return "It's a little chilly out there!"
+#     elif temperature > 85:
+#         return "It's too dang hot out there!"
+#     else:
+#         return "It's perfect out there!"
+    
+
+# def fizzbuzz(num):
+#     # your code here
+#     if num % 3 == 0 and  num % 5 == 0:
+#         return  "FizzBuzz"
+#     elif num % 3 == 0:
+#         return "Fizz"
+#     elif num % 5 == 0:
+#         return "Buzz"
+#     else:
+#         return num
+    
+
+# def calculator(operation, num1, num2):
+#     # your code here
+#     if operation == "+":
+#         return num1 + num2
+#     elif operation == "-":
+#         return num1 - num2
+#     elif operation == "/":
+#         return num1 / num2
+#     elif operation == "*":
+#         return num1 * num2
+#     else:
+#         print("Invalid operation!")
+#         return None
+
+# def happy_new_year():
+#     # code goes here!
+#     counter = 
+#     while counter > 0:
+#         print(counter)
+#         counter -=1
+#     print("Happy New Year!")
+# printe = happy_new_year()
+# print(printe)
+# def make_exclamation(sentence_list):
+#     return [sentence_list]
+
+# result = make_exclamation([f"Hello", f"I'm doing great", f"Python is fun"])
+# print(result)
+
+
+# def lunch_menu (["pizza", "sandwich", "sushi", "soup", "salad"]) = ["pizza", "sandwich", "sushi", "soup", "salad"]
+
+#  for item in lunch_item:
+#     print(item + "!")
+
+
+# print(lunch_menu + "!")
+
---
+++ F3/practice/app.py
@@ -0,0 +1,15 @@
+import sys
+
+
+
+def say_hi():
+    print("Hi")
+
+
+def add_this(x,y):
+    print(x + y)
+
+
+if _name_ == '_app_':
+   args = sys.argv[1:]
+# print(args)
